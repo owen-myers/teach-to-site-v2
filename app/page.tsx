cb50a98c7e7e7ef679c0ceedc16a5510e2bf7551
owen-myers <74884436+owen-myers@users.noreply.github.com>
2024-03-16 13:27:10 -0400

    Home page mobile responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="container mx-auto mt-40 max-w-sm">
+    <div className="container mx-auto w-3/4 text-left mt-40 max-w-sm">
       <h1 className="font-averia text-xl mb-6">Advisory services for non-profits taking innovative leaps.</h1>
       <br></br>
       <p className="font-chivo text-sm mb-6">We bring simple frameworks for innovation
