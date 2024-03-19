bd1989ac4f29cca4c47180c52cac3202b52b8254
owen-myers <74884436+owen-myers@users.noreply.github.com>
2024-03-19 12:07:20 -0400

    Changing <Nav/> responsiveness
    Update to <Nav/> that makes it fit on top of the screen on mobile.

--- app/nav.tsx
+++ app/nav.tsx
@@ -4,12 +4,12 @@ export default function Nav() {
     return (
         <div>
             <nav className="font-averia text-black py-8 flex justify-between items-center">
-                <div className="ml-12 mr-8">
+                <div className="ml-12 mr-3 sm:mr-8">
                 <Link href="/">TeachTo</Link>
                 </div>
                 <div>
-                    <Link href="/philosophy" className="mr-8 transition-colors duration-300 ease-in-out hover:text-gray-500 focus:text-gray-500 active:text-gray-500">Philosophy</Link>
-                    <Link href="/work" className="mr-8 transition-colors duration-300 ease-in-out hover:text-gray-500 focus:text-gray-500 active:text-gray-500">Work</Link>
+                    <Link href="/philosophy" className="mr-3 sm:mr-8 transition-colors duration-300 ease-in-out hover:text-gray-500 focus:text-gray-500 active:text-gray-500">Philosophy</Link>
+                    <Link href="/work" className="mr-3 sm:mr-8 transition-colors duration-300 ease-in-out hover:text-gray-500 focus:text-gray-500 active:text-gray-500">Work</Link>
                     <Link href="/contact" className="mr-12 transition-colors duration-300 ease-in-out hover:text-gray-500 focus:text-gray-500 active:text-gray-500">Contact</Link>
                 </div>
             </nav>
