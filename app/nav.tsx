c3a75804f93f32105ae297968336fa71cb9cc6af
owen-myers <74884436+owen-myers@users.noreply.github.com>
2024-03-19 11:49:41 -0400

    UI updates
    Increased response window to 1024px and made links appear gray when clicked.

--- app/nav.tsx
+++ app/nav.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export default function Nav() {
     return (
         <div>
-            <nav className="font-averia text-black p-8 flex justify-between items-center">
-                <div className="ml-8">
+            <nav className="font-averia text-black py-8 flex justify-between items-center">
+                <div className="ml-12 mr-8">
                 <Link href="/">TeachTo</Link>
                 </div>
                 <div>
-                    <Link href="/philosophy" className="mr-8">Philosophy</Link>
-                    <Link href="/work" className="mr-8">Work</Link>
-                    <Link href="/contact" className="mr-8">Contact</Link>
+                    <Link href="/philosophy" className="mr-8 transition-colors duration-300 ease-in-out hover:text-gray-500 focus:text-gray-500 active:text-gray-500">Philosophy</Link>
+                    <Link href="/work" className="mr-8 transition-colors duration-300 ease-in-out hover:text-gray-500 focus:text-gray-500 active:text-gray-500">Work</Link>
+                    <Link href="/contact" className="mr-12 transition-colors duration-300 ease-in-out hover:text-gray-500 focus:text-gray-500 active:text-gray-500">Contact</Link>
                 </div>
             </nav>
         </div>
