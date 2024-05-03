b93c7424f9d5f492a81443fccee1b340475a993f
owen-myers <74884436+owen-myers@users.noreply.github.com>
2024-05-03 13:11:09 -0400

    updated copy 2.0

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@
 export default function Home() {
   return (
     <div className="container mx-auto w-3/4 text-left mt-40 max-w-sm">
-      <h1 className="font-averia text-xl mb-6">From good intentions to action</h1>
+      <h1 className="font-averia text-xl mb-6">Iuka is an innovation partner for non-profits</h1>
       <br></br>
-      <p className="font-chivo text-sm mb-6">Iuka does user research for impact organizations. We work alongside you to refine your idea, design with end users,
-        and maximize the effectiveness of philanthropic dollars. 
+      <p className="font-chivo text-sm mb-6">We work with you to refine your idea, design with end users,
+        and maximize the effectiveness of philanthropic dollars.
       </p>
     </div>
   );
