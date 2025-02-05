054e3048d8b0c4ca49df61297a70ac7371ba3ce7
owen-myers <74884436+owen-myers@users.noreply.github.com>
2025-02-05 16:57:22 -0500

    updating copy and navbar

--- app/nav.tsx
+++ app/nav.tsx
@@ -5,7 +5,9 @@ export default function Nav() {
         <div>
             <nav className="font-averia text-black py-8 flex justify-between items-center">
                 <div className="ml-12 mr-3 sm:mr-8">
-                <Link href="/">Iuka</Link>
+                <Link href="/">Iuka
+                <p className="font-chivo text-gray-500 text-xs">(eye-you-kuh)</p>
+                </Link>
                 </div>
                 <div>
                     <Link href="/philosophy" className="mr-3 sm:mr-8 transition-colors duration-300 ease-in-out hover:text-gray-500 focus:text-gray-500 active:text-gray-500">Philosophy</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: {
     template: "%s | Iuka Innovation",
-    default:"Iuka - Innovation Partners in Columbus, OH",
+    default:"Iuka - Innovation Partners in NYC",
   },
   description: `Iuka brings innovation frameworks to impact organizations.
   Embedded partnerships to meet users, identify opportunites, and maximize
@@ -14,10 +15,18 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="container mx-auto w-3/4 text-left mt-40 max-w-sm">
-      <h1 className="font-averia text-xl mb-6">Iuka builds products and programs for impact organizations</h1>
-      <p className="font-chivo text-sm mb-6">We work with you to refine your idea, design with end users,
-        and maximize the effectiveness of philanthropic dollars.
+      <h1 className="font-averia text-2xl mb-6">Build products and programs that truly improve the common welfare</h1>
+      <p className="font-chivo text-sm mb-8">
+        Research and prototyping services for teams innovating in the care economy. 
+        Now accepting clients for 2025. 
       </p>
+      <div className="flex justify-center">
+        <Link href="/work">
+        <button className='rounded border border-black font-chivo text-md
+        transition duration-300 ease-in-out transform hover:bg-gray-100 
+        hover:scale-105 p-2'>Work &rarr;</button>
+        </Link>
+      </div>
     </div>
   );
 };
