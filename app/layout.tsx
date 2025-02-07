13420308d39ab60b86689689e803e9b60fab0806
owen-myers <74884436+owen-myers@users.noreply.github.com>
2025-02-07 17:48:51 -0500

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,25 @@ import Image from "next/image";
 import TeachToScribble from "./TeachTo_Scribble_Transparent.png";
 import Nav from "./nav";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default:"Iuka | Innovation Partners in NYC",
+    template: "%s | Iuka Innovation",
+  },
+  description: `Iuka brings innovation frameworks to impact organizations.
+  Embedded partnerships to meet users, identify opportunites, and maximize
+  philanthropic impact.`,
+  metadataBase: new URL ("https://iukaimpact.com")
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+})
+ {
   return (
     <html lang="en">
       <body >
