264cb885a59946bc939bdb8c6763d3090b1f5d21
owen-myers <74884436+owen-myers@users.noreply.github.com>
2025-02-23 23:05:11 -0500

    added new teachto card and link

--- app/card-comp.tsx
+++ app/card-comp.tsx
@@ -3,18 +3,23 @@ import Image from "next/image";
 interface CardProps {
     title: string;
     subtitle: string;
+    showArrow: boolean;
 };
 
-const Card: React.FC<CardProps> =({ title, subtitle }) => {
+const Card: React.FC<CardProps & { showArrow?: boolean }> = ({ title, subtitle, showArrow }) => {
     return (
-        <div className="max-w-72 max-h-100 rounded border border-black overflow-hidden 
+        <div className="relative max-w-72 max-h-100 rounded border border-black overflow-hidden 
         transition duration-300 ease-in-out transform hover:bg-gray-100 hover:scale-105">
+            {showArrow && (
+                <button className="absolute top-1 right-2 text-xl">{"\u2197"}</button>
+            )}
             <div className="px-6 py-4">
-                <div className="font-castoro text-xl mb-4">{title}</div>
-                <p className="font-chivo text-sm">{subtitle}</p>
+                <div className="font-castoro text-2xl mb-4">{title}</div>
+                <p className="font-chivo text-md">{subtitle}</p>
             </div>
         </div>
     );
 };
+
 
 export default Card;
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -10,11 +10,19 @@ export const metadata: Metadata = {
 
 export default function Work() {
  return (
-        <div className ="flex justify-center items-center mt-40 mb-14">
+        <div className ="flex justify-center items-center mt-40 mb-14 space-x-4">
             <Link href="/work/givebackkids">
                 <Card
-                    title="GiveBackKids"
-                    subtitle="Teaching design thinking to five-year-olds"
+                 title="GiveBackKids"
+                 subtitle="Teaching design thinking to five-year-olds"
+                 showArrow={false}
+                />
+            </Link>
+            <Link href="https://www.teach-to.com" target="_blank" rel="noopener noreferrer">
+                <Card
+                 title="TeachTo"
+                 subtitle="An IEP goal-writing prototype for SPED professionals"
+                 showArrow={true}
                 />
             </Link>
         </div>
