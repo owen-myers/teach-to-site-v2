ed0197ab548b1d5bda8ab7a45b31d6650d217b72
owen-myers <74884436+owen-myers@users.noreply.github.com>
2025-03-30 16:21:52 -0400

    update link in work page

--- app/work/givebackkids/page.tsx
+++ app/work/givebackkids/page.tsx
@@ -20,7 +20,7 @@ export default function GiveBackKids() {
     return (
         <div>
             <div className="max-w-3xl px-4 mx-auto mt-8 mb-6">
-                <a href="/work" className="font-rethink text-gray-500 hover:text-gray-900 transition duration-200 ease-in-out">&larr; Back</a>
+                <a href="/" className="font-rethink text-gray-500 hover:text-gray-900 transition duration-200 ease-in-out">&larr; Back</a>
             </div>
             <div className="max-w-3xl mx-auto px-4">
                 <div>
