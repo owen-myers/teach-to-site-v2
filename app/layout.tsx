f5f0d669231d2df1e25aa7387fc5226397be9872
owen-myers <74884436+owen-myers@users.noreply.github.com>
2025-04-08 22:16:15 -0400

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,24 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://www.punchcardproduct.com",
   },
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      {
+        rel: 'mask-icon',
+        url: '/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
   openGraph: {
     type: "website",
     locale: "en_US",
